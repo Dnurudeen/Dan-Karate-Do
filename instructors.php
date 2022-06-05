4bc65f1647a938a642c74e2ca9c9279090e5c535
Dnurudeen <danijunurdeen@gmail.com>
2022-06-05 11:56:15 +0100

    Instructors Page

---
+++ instructors.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.3.2, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-removebg-preview-122x122.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Club Instructors</title>
+  <link rel="stylesheet" href="instructassets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="instructassets/tether/tether.min.css">
+  <link rel="stylesheet" href="instructassets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="instructassets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="instructassets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="instructassets/theme/css/style.css">
+  <link rel="stylesheet" href="instructassets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+<section class="features12 cid-t7ScBjHWU2" id="features12-2" data-rv-view="18">
+    
+    <div class="container">
+        <h2 class="mbr-section-title pb-2 mbr-fonts-style display-2"><strong>CLUB INSTRUCTORS</strong></h2>
+        <h3 class="mbr-section-subtitle pb-3 mbr-fonts-style display-5">Dan Karate-Do presents her Dojo Instructors with a brief profile</h3>
+
+        <div class="media-container-row pt-5">
+            <div class="block-content align-right">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                             <span class="mbr-iconfont mbri-user" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                               <strong>Founder | Chief Instructor</strong> </h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">Origin Name:&nbsp;<br><strong><i> Abdullateef Daniju</i><br></strong><br>Dojo Name:&nbsp;<br><strong><i>Instructor Habdul</i> </strong></p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                            <span class="mbr-iconfont mbri-magic-stick" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                               <strong>Strength/Ability</strong> </h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Strong<br><br>Smart<br><br>Multitasking</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mbr-figure" style="width: 48%;">
+                <img src="instructassets/images/dan23-1440x1800.jpg" alt="Instructor Habdul" title="" media-simple="true">
+            </div>
+
+            <div class="block-content align-left  ">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                             <span class="mbr-iconfont mbri-growing-chart" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                               <strong> Belt Rank</strong></h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Black belt II (Nidan)<br><br><br><br><br></p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                            <span class="mbr-iconfont mbri-globe-2" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7"><strong> Social Media Handles</strong> </h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7"><strong>Linkedin: <a href="#" class="text-secondary">daniju_abdullateef</a></strong><br><br><strong>Facebook:</strong> <a href="#" class="text-secondary">daniju_abdullateef</a><br><br><strong>Instagram:</strong> <a href="#" class="text-secondary">daniju_abdullateef</a><strong><a href="#" class="text-secondary"><br></a></strong></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="engine"><a href="https://mobirise.co/p">bootstrap menu</a></section><section class="features12 cid-t7Sk49M6S6" id="features12-3" data-rv-view="21">
+    
+    
+
+    
+
+    <div class="container">
+        <h2 class="mbr-section-title pb-2 mbr-fonts-style display-2"></h2>
+        
+
+        <div class="media-container-row pt-5">
+            <div class="block-content align-right">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                             <span class="mbr-iconfont mbri-user" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7"><strong> Developer | Senior Instructor</strong></h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">Origin Name:&nbsp;<br><strong><i> Nurdeen Daniju</i><br></strong><br>Dojo Name:&nbsp;<br><strong><i>Instructor Deen</i> </strong></p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                            <span class="mbr-iconfont mbri-magic-stick" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                               <strong>Strength/Ability</strong> </h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Flexible<br><br>Fast<br><br>Skilled<br></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mbr-figure" style="width: 50%;">
+                <img src="instructassets/images/dan24-1440x1800.jpg" alt="Instructor Deen" title="" media-simple="true">
+            </div>
+
+            <div class="block-content align-left  ">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                             <span class="mbr-iconfont mbri-growing-chart" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                               <strong>Belt Rank</strong> </h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Brown belt III<br><br><br><br><br></p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                            <span class="mbr-iconfont mbri-globe-2" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7"><strong>Social Media Handles</strong> </h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7"><strong>Linkedin: <a href="#" class="text-secondary">daniju_deen</a></strong><br><br><strong>Facebook:</strong> <a href="#" class="text-secondary">daniju_deen</a><br><br><strong>Instagram:</strong> <a href="#" class="text-secondary">daniju_deen</a><strong><a href="#" class="text-secondary"><br></a></strong></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="features12 cid-t7SlmNyh2a" id="features12-4" data-rv-view="24">
+    
+    
+
+    
+
+    <div class="container">
+        <h2 class="mbr-section-title pb-2 mbr-fonts-style display-2"></h2>
+        
+
+        <div class="media-container-row pt-5">
+            <div class="block-content align-right">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                             <span class="mbr-iconfont mbri-user" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                               <strong> Planner | Senior Instructor</strong></h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">Origin Name:&nbsp;<br><strong><i> Hanat Daniju</i><br></strong><br>Dojo Name:&nbsp;<br><strong><i> Instructor Hanat</i></strong></p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                            <span class="mbr-iconfont mbri-magic-stick" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                               <strong> Strength/Ability</strong></h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">Calm<br><br>Fast<br><br>Loving<br></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mbr-figure" style="width: 50%;">
+                <img src="instructassets/images/dan26-1440x1800.jpg" alt="Instructor Hanat" title="" media-simple="true">
+            </div>
+
+            <div class="block-content align-left  ">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                             <span class="mbr-iconfont mbri-growing-chart" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                               <strong>Belt Rank</strong> </h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Brown belt II<br><br><br><br><br></p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                            <span class="mbr-iconfont mbri-globe-2" media-simple="true"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7"><strong>Social Media Handles</strong> </h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7"><strong>Linkedin: <a href="#" class="text-secondary">daniju_hanat</a></strong><br><br><strong>Facebook:</strong> <a href="#" class="text-secondary">daniju_hanat</a><br><br><strong>Instagram:</strong> <a href="#" class="text-secondary">daniju_hanat</a><strong><a href="#" class="text-secondary"><br></a></strong></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="instructassets/web/assets/jquery/jquery.min.js"></script>
+  <script src="instructassets/popper/popper.min.js"></script>
+  <script src="instructassets/tether/tether.min.js"></script>
+  <script src="instructassets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="instructassets/smooth-scroll/smooth-scroll.js"></script>
+  <script src="instructassets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,81 @@
-/*!
+<section class="features16 cid-t7RN0xFoFy" id="features16-0" data-rv-view="10">
+    
+    
+    
+    <div class="container align-center">
+        <h2 class="pb-3 mbr-fonts-style mbr-section-title display-2"><strong>CLUB INSTRUCTORS</strong></h2>
+        
+        <div class="row media-row">
+            
+        <div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="assets/images/dan23-1440x1800.jpg" alt="Instuctor Habdul" title="" media-simple="true">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">Abdullateef Daniju</p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Founder | Chief Instructor</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/" target="_blank">
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-twitter socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-facebook socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-googleplus socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-linkedin socicon" media-simple="true"></span>
+                            
+                            
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="assets/images/dan24-1440x1800.jpg" alt="Instuctor Deen" title="" media-simple="true">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">Nurudeen Daniju</p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Senior Instructor</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/" target="_blank">
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-twitter socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-facebook socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-googleplus socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-linkedin socicon" media-simple="true"></span>
+                            
+                            
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="assets/images/dan26-1440x1800.jpg" alt="Instuctor Hanat" title="" media-simple="true">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">Hanat Daniju</p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Senior Instructor</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/" target="_blank">
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-twitter socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-facebook socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-googleplus socicon" media-simple="true"></span>
+                            <span class="p-1 mbr-iconfont mbr-iconfont-social socicon-linkedin socicon" media-simple="true"></span>
+                            
+                            
+                        </a>
+                    </div>
+                </div>
+            </div></div>    
+    </div>
+</section>/*!
     * Start Bootstrap - SB Admin v7.0.3 (https://startbootstrap.com/template/sb-admin)
     * Copyright 2013-2021 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
